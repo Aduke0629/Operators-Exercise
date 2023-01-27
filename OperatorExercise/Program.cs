791db424033b4bac1cca5d958d471ba80c84cee6
Aaron Duke <aaron.duke0817@gmail.com>
2023-01-26 19:18:53 -0500

    initial commit

--- OperatorExercise/Program.cs
+++ OperatorExercise/Program.cs
@@ -2,9 +2,49 @@
 {
     public class Program
     {
+        
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+            int a = 17;
+
+            int b = 4;
+
+            int quiotient = a / b;
+
+            int remainder = a % b;
+
+            if (a == 17 && b == 4) ;
+
+            Console.WriteLine($"{a}/{b} is {quiotient} remainder {remainder}");
+
+
+            Console.WriteLine("What is the radius of our circle?");
+
+            var radius = double.Parse(Console.ReadLine());
+
+            Console.WriteLine($"The area of your circle with the radius of {radius} is {AreaOfCircle(radius)}");
+
         }
+
+        public static double AreaOfCircle(double radius)
+        {
+           return Math.PI * (radius * radius);
+
+        }
+        
+
+      
+        
+           
+     
+
+         
+        
+        
+
+        
     }
+   
 }
